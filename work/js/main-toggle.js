c97190eed19d61c54e96eafda80abc29fb80a45f
romeo198808 <romeo1988.08.03@gmail.com>
2019-11-21 14:40:05 +0200

    Разметил страницу "index.html" Начал стилизацию Сделал стили для "header" mobile

---
+++ work/js/main-toggle.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function functionName() {
+  var mainNav = document.querySelector('.main-nav');
+  var toggle = document.querySelector('.main-nav__toggle');
+
+  mainNav.classList.remove('main-nav__nojs');
+
+  toggle.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    if (toggle.classList.contains('main-nav__toggle--close')) {
+      toggle.classList.remove('main-nav__toggle--close');
+      toggle.classList.add('main-nav__toggle--open');
+      mainNav.style="display: block;"
+    } else {
+      toggle.classList.remove('main-nav__toggle--open');
+      toggle.classList.add('main-nav__toggle--close');
+      mainNav.style="display: none;"
+    }
+  });
+})();
